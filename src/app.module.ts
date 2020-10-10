7bae802ceea71d0c914ac68b315f8fcd0a34c4a7
vapvin <vins@vins.dev>
2020-10-10 17:34:54 +0900

    Create Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import {GraphQLModule} from '@nestjs/graphql';
+import {join} from 'path';
+import { RestaurnatsModule } from './restaurnats/restaurnats.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    ),
+    RestaurantsModule},
+    RestaurnatsModule},
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
